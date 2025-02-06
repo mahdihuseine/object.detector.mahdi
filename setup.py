99f1fdc907b0ee8db9865bf23c62b13dfb721b01
mahdihuseine <Mahdihuseine001@gmail.com>
2025-02-06 05:21:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="object.detector.mahdi",  # نام کتابخانه شما
+    name="object_detector",  # نام کتابخانه شما
     version="0.1",
     packages=find_packages(),
     install_requires=[
@@ -11,14 +11,14 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'object-detector-mahdi=object.detector.mahdi.detect:main',  # نام تابع main که در آن کارهای اصلی انجام می‌شود
+            'object-detector=object_detector.detect:main',  # نام تابع main که در آن کارهای اصلی انجام می‌شود
         ],
     },
     include_package_data=True,
     description="A library for car and license plate detection",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/mahdihuseine/object.detector.mahdi",  # آدرس گیت‌هاب یا صفحه پروژه شما
+    url="https://github.com/mahdihuseine/object_detector",  # آدرس گیت‌هاب یا صفحه پروژه شما
     author="mahdihuseine",
     author_email="Mahdihuseine001@gmail.com",
     license="MIT",
